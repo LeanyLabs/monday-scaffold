fa9e8f052d7e646401f5f9d3197133fd3b37e349
Vova <vovadorko@gmail.com>
2021-08-13 14:10:46 +0300

    feat: router was refactored

--- templates/typescript/src/routes/monday.ts
+++ templates/typescript/src/routes/monday.ts
@@ -5,9 +5,20 @@ import authenticationMiddleware from '../middlewares/monday-authentication';
 import prepareMondayMiddleware from '../middlewares/monday-utils';
 import { executeAction, getRemoteListOptions } from '../controllers/monday-controller';
 
-const router = Router().use(authenticationMiddleware).use(prepareMondayMiddleware);
+const router = Router();
 
-router.post('/execute_action', wrapAsync(executeAction));
-router.post('/get_remote_list_options', getRemoteListOptions);
+const triggers = Router().use(authenticationMiddleware).use(prepareMondayMiddleware);
+const actions = Router().use(authenticationMiddleware).use(prepareMondayMiddleware);
+const fieldTypes = Router().use(authenticationMiddleware);
+const webhooks = Router();
+
+actions.post('/execute_action', wrapAsync(executeAction));
+
+fieldTypes.post('/get_remote_list_options', wrapAsync(getRemoteListOptions));
+
+router.use('/triggers', triggers);
+router.use('/actions', actions);
+router.use('field_types', fieldTypes);
+router.use('/webhooks', webhooks);
 
 export default router;
